8db123c7a5272e95c2abea537b3b6e836e937fa4
DiogoDSJ <diogosj2003@gmail.com>
2023-09-08 07:06:16 -0300

    :construction: interface CRUD

--- src/dao/CRUD.java
+++ src/dao/CRUD.java
@@ -1,10 +1,22 @@
 package dao;
 
+import java.util.List;
+
+
 public interface CRUD<T> {
 
     public T create(T obj);
 
     public void delete(T obj);
 
-    public T findById(T id);
+    public T findByPk(T obj);
+
+    public void deleteMany();
+
+    public T update(T obj);
+
+    public List<T> findMany();
+
+
+
 }
