9c3cf238168f400f6aa53ee2eb0177565fba189e
DiogoDSJ <diogosj2003@gmail.com>
2023-09-08 08:33:58 -0300

    :construction: Campo id adicionado na classe Livro

--- src/model/entities/Livro.java
+++ src/model/entities/Livro.java
@@ -2,6 +2,7 @@ package model.entities;
 
 public class Livro {
 
+    private int id;
     private int isbn;
     private String autor;
     private String titulo;
@@ -19,5 +20,51 @@ public class Livro {
         this.categoria = categoria;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
