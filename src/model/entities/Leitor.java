0c0b4b31fe3b426bcbd9c14028aaef3ffd0757ad
DiogoDSJ <diogosj2003@gmail.com>
2023-09-11 18:43:01 -0300

    :bug: fix: Construtor duplicado na classe Leitor

--- src/model/entities/Leitor.java
+++ src/model/entities/Leitor.java
@@ -4,17 +4,15 @@ import model.entities.enums.Cargo;
 
 public class Leitor extends Usuario {
 
-    public Leitor(String nome, String endereco, String telefone, int id, String usuario, String senhaDeAcesso) {
-        super(nome, endereco, telefone, id, usuario, senhaDeAcesso);
     private int numeroDeEmprestimos;
+
     public Leitor(String nome, String endereco, String telefone, String usuario, String senhaDeAcesso) {
         super(nome, endereco, telefone, usuario, senhaDeAcesso);
         this.numeroDeEmprestimos = 3;
         this.setCargo(Cargo.LEITOR);
     }
 
-    public Integer numeroDeEmprestimos(){
-        return 0;
     public Integer getNumeroDeEmprestimos(){
+        return numeroDeEmprestimos;
     }
 }
