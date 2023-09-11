c613b94f5d25aaeeb68327d15df693d4164a3bba
DiogoDSJ <diogosj2003@gmail.com>
2023-09-11 18:41:11 -0300

    :sparkles: feat: Atributo numero de emprestimos adicionado na classe Leitor

--- src/model/entities/Leitor.java
+++ src/model/entities/Leitor.java
@@ -6,12 +6,15 @@ public class Leitor extends Usuario {
 
     public Leitor(String nome, String endereco, String telefone, int id, String usuario, String senhaDeAcesso) {
         super(nome, endereco, telefone, id, usuario, senhaDeAcesso);
+    private int numeroDeEmprestimos;
     public Leitor(String nome, String endereco, String telefone, String usuario, String senhaDeAcesso) {
         super(nome, endereco, telefone, usuario, senhaDeAcesso);
+        this.numeroDeEmprestimos = 3;
         this.setCargo(Cargo.LEITOR);
     }
 
     public Integer numeroDeEmprestimos(){
         return 0;
+    public Integer getNumeroDeEmprestimos(){
     }
 }
