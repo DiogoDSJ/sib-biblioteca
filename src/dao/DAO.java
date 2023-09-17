bb91d61e30936a707d9071cb90155f4663ab966f
DiogoDSJ <107954198+DiogoDSJ@users.noreply.github.com>
2023-09-17 09:36:07 -0300

    :sparkles: feat: EmprestimoDAO adicionado no arquivo DAO.

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -4,6 +4,8 @@ import dao.administrador.AdministradorDAO;
 import dao.administrador.AdministradorDAOList;
 import dao.bibliotecario.BibliotecarioDAO;
 import dao.bibliotecario.BibliotecarioDAOList;
+import dao.emprestimo.EmprestimoDAO;
+import dao.emprestimo.EmprestimoDAOList;
 import dao.leitor.LeitorDAO;
 import dao.leitor.LeitorDAOList;
 import dao.livro.LivroDAO;
@@ -15,6 +17,7 @@ public class DAO {
     private static BibliotecarioDAO bibliotecarioDAO;
     private static LeitorDAO leitorDAO;
     private static LivroDAO livroDAO;
+    private static EmprestimoDAO emprestimoDAO;
     public static AdministradorDAO getAdministradorDAO() {
         if(administradorDAO == null)
         {
@@ -43,6 +46,13 @@ public class DAO {
             livroDAO = new LivroDAOList();
         }
         return livroDAO;
+    }
+
+    public static EmprestimoDAO getEmprestimoDAO(){
+        if(emprestimoDAO == null){
+            emprestimoDAO = new EmprestimoDAOList();
+        }
+        return emprestimoDAO;
     }
 
 }
