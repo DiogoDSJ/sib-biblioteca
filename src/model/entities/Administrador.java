ac36a7fb2e045f3a9e4e857c96d800125ebf1224
DiogoDSJ <107954198+DiogoDSJ@users.noreply.github.com>
2023-09-30 07:17:52 -0300

    :sparkles: feat: Funções de manipulação do leitor.

--- src/model/entities/Administrador.java
+++ src/model/entities/Administrador.java
@@ -32,10 +32,55 @@ public class Administrador extends Bibliotecario {
         }
     }
 
-    public void editarUsuario(String id){
+    public void atualizarUsuario(Usuario usuario) throws objetoInexistenteException{
+        if(usuario == null) throw new objetoInexistenteException("Usuário não existe");
+        else if(usuario.getCargo().equals(Cargo.LEITOR)) {
+            DAO.getLeitorDAO().update((Leitor) usuario);
+        }
+        else if(usuario.getCargo().equals(Cargo.BIBLIOTECARIO)) {
+            DAO.getBibliotecarioDAO().update((Bibliotecario) usuario);
+        }
+        else if(usuario.getCargo().equals(Cargo.ADMINISTRADOR)){
+            DAO.getAdministradorDAO().update((Administrador) usuario);
+        }
 
     }
-    public void removerUsuario(String id){
+    public void removerLeitor(String id) throws objetoInexistenteException, usuarioPendenciasException, foraDeEstoqueException {
+        Leitor leitor = DAO.getLeitorDAO().findByPk(id);
+        if(leitor == null) throw new objetoInexistenteException("Usuário não existe.");
+        else if(DAO.getEmprestimoDAO().findByIdMutuario(id).isEmpty()) throw new usuarioPendenciasException("Usuário tem pendências, não pode ser removido.");
+        else{
+            List<Reserva> reservas = DAO.getReservaDAO().findByIdReservante(id);
+            for (Reserva reserva: reservas) {
+                DAO.getReservaDAO().delete(reserva);
+            }
+            DAO.getMultaDAO().delete(DAO.getMultaDAO().findByIdMutuario(id));
+            DAO.getLeitorDAO().delete(leitor);
+            Sistema.atualizarReservas();
+        }
+    }
 
+    public void trocarUsuarioDoLeitor(String usuario, String novousuario) throws objetoInexistenteException {
+        Leitor leitor = DAO.getLeitorDAO().findByUsuario(usuario);
+        if (leitor == null) throw new objetoInexistenteException("Leitor não existe.");
+        DAO.getLeitorDAO().findByUsuario(usuario).setUsuario(novousuario);
+    }
+
+    public void trocarSenhaDoLeitor(String usuario, String novasenha) throws objetoInexistenteException {
+        Leitor leitor = DAO.getLeitorDAO().findByUsuario(usuario);
+        if (leitor == null) throw new objetoInexistenteException("Leitor não existe.");
+        DAO.getLeitorDAO().findByUsuario(usuario).setSenhaDeAcesso(novasenha);
+    }
+
+    public void trocarTelefoneDoLeitor(String usuario, String novotelefone) throws objetoInexistenteException {
+        Leitor leitor = DAO.getLeitorDAO().findByUsuario(usuario);
+        if (leitor == null) throw new objetoInexistenteException("Leitor não existe.");
+        DAO.getLeitorDAO().findByUsuario(usuario).setTelefone(novotelefone);
+    }
+
+    public void trocarEnderecoDoLeitor(String usuario, String novoendereco) throws objetoInexistenteException {
+        Leitor leitor = DAO.getLeitorDAO().findByUsuario(usuario);
+        if (leitor == null) throw new objetoInexistenteException("Leitor não existe.");
+        DAO.getLeitorDAO().findByUsuario(usuario).setTelefone(novoendereco);
     }
 }
