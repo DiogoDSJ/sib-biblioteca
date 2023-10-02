09032d281a606e020ca1369998a60399ca54e17a
DiogoDSJ <107954198+DiogoDSJ@users.noreply.github.com>
2023-10-02 08:40:37 -0300

    :books: docs: Documentação da classe Empréstimo.

--- src/model/entities/Emprestimo.java
+++ src/model/entities/Emprestimo.java
@@ -7,6 +7,12 @@ import exceptions.usuarioBloqueadoException;
 
 import java.time.LocalDate;
 
+/**
+ * Classe empréstimo que ira guardar as informações um empréstimo de um livro para o leitor.
+ *
+ * Aqui a classe guarda as datas de inicio e fim do empréstimo, o id do leitor que solicitou o empréstimo, o isbn do
+ * livro que foi emprestado e o id do empréstimo que foi gerado pelo DAO.
+ */
 public class Emprestimo {
     private final LocalDate dataInicio;
     private LocalDate dataFim;
@@ -14,9 +20,17 @@ public class Emprestimo {
     private final String isbnLivro;
     private String idEmprestimo;
 
+    /**
+     * Construtor padrão da classe empréstimo, aqui é criado o empréstimo já com a dataInicio que é a momento que foi
+     * criado e a data fim predefinida para daqui a 7 dias da data inicio, aqui o leitor perde uma unidade de emprés-
+     * timo e o livro perde uma quantidade no estoque.
+     * @param idMutuario id do usuário que dono do empréstimo.
+     * @param isbnLivro isbn do livro do empréstimo.
+     * @throws foraDeEstoqueException Caso o livro não haja estoque.
+     */
     public Emprestimo(String idMutuario, String isbnLivro) throws foraDeEstoqueException {
         this.dataInicio = LocalDate.now();
-        this.dataFim = dataInicio.plusDays(7);
+        this.dataFim = this.dataInicio.plusDays(7);
         this.idMutuario = idMutuario;
         this.isbnLivro = isbnLivro;
         DAO.getLeitorDAO().findByPk(idMutuario).removerUmEmprestimo();
